1544cac8f72eec7f2a877370eb861693ed3b3c3a
synetcom1 <tech.headinstructor@ciccc.ca>
2020-05-05 19:29:08 -0700

    php1

---
+++ php/index4.php
@@ -0,0 +1,22 @@
+<?php 
+// constant function (parenthesis)
+
+define('NAME', 'Steve');
+// define variable with $sign, but constant using define function(almost smae as python)
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php 
+    echo NAME;
+    ?>
+    </h1>
+</body>
+</html>

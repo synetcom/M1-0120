bef7136ab3ea316d3677c7dfdd239f58d03d0e08
synetcom1 <tech.headinstructor@ciccc.ca>
2020-05-15 09:31:49 -0700

    fixed db connection

--- php/details.php
+++ php/details.php
@@ -1,70 +1,72 @@
 <?php 
+include('config/db_connect.php');
 
-// delete content
+// Delete content
 if(isset($_POST['delete'])){
     $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
 
-$sql = "DELETE FROM users WHERE id =$id_to_delete";
-if(mysqli_query($conn, $sql)){
-    //success
-    header('Location: index.php');
-}else{
-    // failure
-    echo 'query error'. mysqli_error($conn);
+    $sql = "DELETE FROM users WhERE id =$id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+        // success
+        header('Location:index.php');
+    }else{
+        // failure
+        echo 'query error:' . mysqli_error($conn);
+    }
 }
-    
-}
 
 
 
-// checking GET request id parameter
+// Check GET request id param
 if(isset($_GET['id'])){
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
-
     // make sql
-    $sql = "SELECT * FROM users WHERE id = $id";
+    $sql = "SELECT * FROM users WHERE id= $id";
 
-    // query result
+    // get query result
     $result = mysqli_query($conn, $sql);
 
 
     // fetch result in array format
-    $user =mysqli_fetch_assoc($result);
 
-    // free the result
-    mysqli_free_result($result);
-    mysqli_close($conn);
+$user = mysqli_fetch_assoc($result);
 
+mysqli_free_result($result);
+mysqli_close($conn);
+
+// print_r($user);
 }
 
-
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+<?php include ('./templates/header.php');?>
+<div class="ciontainer center">
 
-<?php include ('./templates/header.php')?>;
-<div class="container center">
-    <?php if($user): ?>
-<h4><?php echo htmlspecialchars($user['title']); ?></h4>
-<p>Created by: <?php echo htmlspecialchars($user['email']); ?></p>
-<p> <?php echo htmlspecialchars($user['created_at']); ?></p>
+<?php if($user): ?>
+
+    <h4><?php echo htmlspecialchars($user['title']);?></h4>
+<p>Created by: <?php htmlspecialchars($pizza['email']); ?></p>
+<p><?php echo date($user['created_at']); ?></p>
 <h5>Contents:</h5>
-<p><?php echo htmlspecialchars($user['content']); ?></p>
+<p><?php echo htmlspecialchars($user['content']) ?></p>
 
-<!-- Delete FORM -->
+<!-- DELETE FORM -->
 <form action="details.php" method="POST">
     <input type="hidden" name="id_to_delete" value="<?php echo $user['id'] ?>">
-    <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0"> 
+
+
 </form>
-    <?php else: ?>
-<h5>No such user exists</h5>
-    <?php endif;?>
 
+
+
+
+<?php else: ?>
+    <h5>No such user exists</h5>
+<?php endif; ?>
 </div>
-
-<?php include ('./templates/footer.php')?>;
+<?php include ('./templates/footer.php');?>
 </html>
--- php/index.php
+++ php/index.php
@@ -1,10 +1,10 @@
-<?php
-// connect to database using include meethod
-
+<?php 
+// Connect to database
+// MySQLi or PDO
 include('./config/db_connect.php');
 
-// writr query for all data
-$sql = 'SELECT title, content, email FROM users ORDER BY created_at';
+// write query for all data
+$sql ='SELECT title, content, id FROM users ORDER BY created_at';
 
 // make query and get result
 $result = mysqli_query($conn, $sql);
@@ -12,38 +12,63 @@ $result = mysqli_query($conn, $sql);
 // fetch the resulting rows as an array
 $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-// free the result from memory
+// free result from memory
 mysqli_free_result($result);
 
 // close connection
 mysqli_close($conn);
 
+// print_r($users);
+
+// separate data by comma
+// print_r(explode(',', $users[0]['content']));
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php include('./templates/header.php'); ?>
+<?php include('./templates/header.php') ?>
+
 <h4 class="center grey-text">Users</h4>
 <div class="container">
-    <div class="row">
-        <?php foreach ($users as $user) { ?>
-            <div class="col s6 md3">
-                <div class="card z-deptph-0">
-                    <div class="card-content center">
-                        <h6><?php echo htmlspecialchars($user['title']); ?></h6>
-                        <div><?php echo htmlspecialchars($user['content']); ?></div>
-                    </div>
-                    <div class="card-action right-align">
-                        <a class="brand-text" href="./details.php?id=<?php echo $user['id'] ?>">more info</a>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
+<div class="row">
+    <!-- start of for each -->
+<?php foreach ($users as $user): ?>
+
+<div class="col s6 md3">
+<div class="card z-depth-0">
+<div class="card-content center">
+<h6><?php  echo htmlspecialchars($user['title']);?></h6>
+
+<!-- separate by comma -->
+<!-- start of for each -->
+<ul><?php foreach(explode(',', $user['content']) as $con): ?>
+<li><?php echo htmlspecialchars($con); ?></li>
+<!-- end of foreach -->
+<?php endforeach; ?>
+</ul>
+
+
+</div>
+<div class="card-action right-align">
+
+<!-- Adding hyperlink -->
+<a class="brand-text" href="./details.php?id=<?php echo $user['id'] ?>">more info</a></div>
+
+</div>
 </div>
 
+<?php endforeach; ?> <!-- end of foreach -->
 
-<?php include('./templates/footer.php'); ?>
+<?php if(count($users) >= 3): ?>
+<p>There are two or more contents.</p>
+<?php  else:  ?>
+<p>There are less than two contents</p>
+<?php endif;?>
+
+</div>
+</div>
+
+<?php include('./templates/footer.php') ?>
 
 </body>
 
